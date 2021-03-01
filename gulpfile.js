f2a33b0cc3619778a78ef17bca62ba24941bdbda
oscardenas <oscardenas17@gmail.com>
2021-02-28 22:18:12 -0500

    gulp - Watch para automatizar

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
-const {series, src, dest} = require('gulp');
+const {series, src, dest, watch} = require('gulp');
 const sass = require('gulp-dart-sass');
 
-//Funcion que compila SASS - series: ejecutar varias funciones - src, ruta archivo sass / dest, donde guardar css compilado
+//Funcion que compila SASS - series: ejecutar varias funciones - src, ruta archivo sass / dest, donde guardar css compilado / watch complia cambios
 
 function css(){
     return src('src/scss/app.scss')
@@ -12,8 +12,22 @@ function css(){
    
 }
 
+function minificarcss(){
+    return src('src/scss/app.scss')
+        .pipe( sass({
+            outputStyle: 'compressed'
+        }    ))
+        .pipe( dest('./build/css') )
+}
+
+function watchArchivos(){
+   watch( 'src/scss/app.scss' , css)
+}
+
 // minificar css
-// outputStyle: 'expanded'
+// outputStyle: 'compressed'
 
 //Compilar con:  gulp css
 exports.css = css;
+exports.minificarcss = minificarcss;
+exports.watchArchivos = watchArchivos
