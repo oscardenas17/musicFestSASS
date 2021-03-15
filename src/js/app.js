825f88b2f092293799d65a09bae572e8d6d5e98d
oscardenas <oscardenas17@gmail.com>
2021-03-14 21:45:45 -0500

    Intersection Oberver para fijar la navegación

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,30 @@
 
 document.addEventListener('DOMContentLoaded', function(){
     scrollNav();
+
+    navegacionFija();
 });
+
+function navegacionFija(){
+
+    const barra = document.querySelector('.header');
+
+    //Resgitar el intersection observer
+    const observer = new IntersectionObserver( function(entries){
+        // console.log(entries[0]);
+        if(entries[0].isIntersecting){
+            // console.log('visble');
+            barra.classList.remove('fijo');
+        }else{
+            // console.log('no visble');
+            barra.classList.add('fijo');
+        }
+    });
+    
+    //Elemento a observar
+    observer.observe(document.querySelector('.sobre-festival'));
+
+}
 
 function scrollNav(){
     const enlaces = document.querySelectorAll('.navegacion-principal a'); 
@@ -19,5 +42,4 @@ function scrollNav(){
         });
     });
 
-   
 }
